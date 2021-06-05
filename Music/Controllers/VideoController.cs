d181e253830fc2b29d22be72497cf10110f44e35
rhyswang <wrusheng2019@outlook.com>
2021-06-05 16:29:44 +0800

    就这样吧

--- Music/Controllers/VideoController.cs
+++ Music/Controllers/VideoController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Music.Controllers
 {
+    /// <summary>
+    /// 视频切片命令:ffmpeg -i {本地视频.ts} -c copy -map 0 -f segment -segment_list {视频索引文件.m3u8} -segment_time 5 {前缀}-%03d.ts
+    /// </summary>
     public class VideoController : Controller
     {
         public IActionResult Index()
